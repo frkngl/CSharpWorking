26882da7a1d198690b32d67889bb772ad9f3b197
frkngl <frknglww@gmail.com>
2025-11-28 21:52:07 +0300

    Double, Char ve Klavyeden veri girişleri String değişkenler

---
+++ Variables/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Double Değişken
+            //double number;
+            //number = 12.5;
+            //Console.WriteLine(number);
+            //Console.Read();
+
+            //double patotaPrice, applePrice, bananaPrice, strawberryPrice, orangePrice;
+
+            //patotaPrice = 25.50;
+            //applePrice = 15.75;
+            //bananaPrice = 18.25;
+            //strawberryPrice = 30;
+            //orangePrice = 22.40;
+
+            //double potatoGram = 2.576;
+            //double appleGram = 1.543;
+            //double bananaGram = 2.153;
+            //double strawberryGram = 0.754;
+            //double orangeGram = 1.889;
+
+            //double potatoTotalPrice = patotaPrice * potatoGram;
+            //double appleTotalPrice = applePrice * appleGram;
+            //double bananaTotalPrice = bananaPrice * bananaGram;
+            //double strawberryTotalPrice = strawberryPrice * strawberryGram;
+            //double orangeTotalPrice = orangePrice * orangeGram;
+
+            //double totalAmount = potatoTotalPrice + appleTotalPrice + bananaTotalPrice + strawberryTotalPrice + orangeTotalPrice;
+
+            //Console.WriteLine("***** Alışveriş Detayları *****");
+            //Console.WriteLine();
+            //Console.WriteLine("Patates: " + potatoGram + " kg x " + patotaPrice + " TL = " + potatoTotalPrice + " TL");
+            //Console.WriteLine("Elma: " + appleGram + " kg x " + applePrice + " TL = " + appleTotalPrice + " TL");
+            //Console.WriteLine("Muz: " + bananaGram + " kg x " + bananaPrice + " TL = " + bananaTotalPrice + " TL");
+            //Console.WriteLine("Çilek: " + strawberryGram + " kg x " + strawberryPrice + " TL = " + strawberryTotalPrice + " TL");
+            //Console.WriteLine("Portakal: " + orangeGram + " kg x " + orangePrice + " TL = " + orangeTotalPrice + " TL");
+            //Console.WriteLine();
+            //Console.WriteLine("Toplam Tutar: " + totalAmount + " TL");
+            //Console.Read();
+            #endregion
+
+            #region Char Değişken
+            //char symbol = 'F'; // Tek karakterlik veri tipidir. 1'den fazla karakter almaz. Tek tırnak içinde tanımlanır.
+            //Console.WriteLine(symbol);
+            //Console.Read();
+            #endregion
+
+            #region Klavyeden Veri Girişleri String Değişkenler
+            //Console.WriteLine("CSharp Kursuna Hoşgeldiniz!");
+            //Console.WriteLine();
+
+            //string customerName, customerSurname, customerPhoneNumber, customerEmail, customerCity, customerDistrict;
+
+            //Console.Write("Yolcu Adı: ");
+            //customerName = Console.ReadLine();
+
+            //Console.Write("Yolcu Soyadı: ");
+            //customerSurname = Console.ReadLine();
+
+            //Console.Write("Yolcu Telefon Numarası: ");
+            //customerPhoneNumber = Console.ReadLine();
+
+            //Console.Write("Yolcu E-Posta Adresi: ");
+            //customerEmail = Console.ReadLine();
+
+            //Console.Write("Yolcu Şehir: ");
+            //customerCity = Console.ReadLine();
+
+            //Console.Write("Yolcu İlçe: ");
+            //customerDistrict = Console.ReadLine();
+
+            //Console.WriteLine();
+            //Console.WriteLine("***** Yolcu Bilgileri *****");
+            //Console.WriteLine("Yolcu Adı: " + customerName + " - Yolcu Soyadı: " + customerSurname + " - Yolcu Telefon Numarası:" + customerPhoneNumber + " - Yolcu Maili: " + customerEmail + " - Yolcu Yaşadığı Şehir: " + customerCity + " - Yolcu İlçesi: " + customerDistrict);
+            #endregion
+
+        }
+    }
+}
