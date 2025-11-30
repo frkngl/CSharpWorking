c2b98b028575ef3fc45571168cf4d7ab29d196b7
frkngl <frknglww@gmail.com>
2025-11-30 22:11:43 +0300

    if Else yapısı işlenildi ve pekiştirildi.

---
+++ 03_MakingDecision/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_MakingDecision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region if Else Yapısı
+
+            //Console.WriteLine("Lütefen şifrenizi giriniz:");
+            //string password;
+            //password = Console.ReadLine();
+
+            //if (password == "abcd")
+            //{
+            //    Console.WriteLine("Şifreniz doğru");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Şifreniz yanlış");
+            //}
+            //Console.ReadLine();
+
+            //-------------------------------------------------------------
+
+            //string country;
+            //string capital;
+
+            //Console.Write("Lütfen bir ülke ismi giriniz: ");
+            //country = Console.ReadLine();
+
+            //Console.Write("Lütfen başkent ismini giriniz: ");
+            //capital = Console.ReadLine();
+
+            //if (country == "türkiye" && capital == "ankara")
+            //{
+            //    Console.WriteLine("veriler doğrulandı.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("veriler hatalı");
+            //}
+            //Console.ReadLine();
+
+            //-------------------------------------------------------------
+
+            //int number;
+            //Console.Write("Sayı giriniz: ");
+            //number = int.Parse(Console.ReadLine());
+            //if(number == 5)
+            //{
+            //    Console.WriteLine("Girilen sayı doürudur.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Girilen sayı yanlıştır.");
+            //}
+            //Console.Read();
+
+            //-------------------------------------------------------------
+
+            //int exam1, exam2, exam3, average;
+            //string result = "Hata";
+
+            //Console.Write("1. Sınav notunuzu giriniz: ");
+            //exam1 = int.Parse(Console.ReadLine());
+
+            //Console.Write("2. Sınav notunuzu giriniz: ");
+            //exam2 = int.Parse(Console.ReadLine());
+
+            //Console.Write("3. Sınav notunuzu giriniz: ");
+            //exam3 = int.Parse(Console.ReadLine());
+
+            //average = (exam1 + exam2 + exam3) / 3;
+
+            //if (average > 0 & average <= 50)
+            //{
+            //    result = "Kaldı";
+            //}
+            //if (average > 50 & average <= 75)
+            //{
+            //    result = "Orta";
+            //}
+            //if (average > 75 & average <= 85)
+            //{ 
+            //    result = "İyi";
+            //}
+            //if (average > 85)
+            //{
+            //    result = "Çok iyi";
+            //}
+
+            //Console.WriteLine(result);
+            //Console.Read();
+
+            //-------------------------------------------------------------
+
+            //string city;
+            //Console.Write("Lütfen bir şehir ismi giriniz: ");
+            //city = Console.ReadLine();
+
+            //if (city == "ankara" | city == "adana" | city == "trabzon" | city == "bursa")
+            //{
+            //    Console.WriteLine("Girdiğiniz şehir bulunmaktadır.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Girdiğiniz şehir bulunmamaktadır.");
+            //}
+
+            //Console.Read();
+
+            //-------------------------------------------------------------
+
+            //string username;
+            //Console.Write("Kullanıcı adınızı giriniz: ");
+            //username = Console.ReadLine();
+
+            //if (username != "admin")
+            //{
+            //    Console.WriteLine("Kullanıcı adınız yanlış.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Hoşgeldiniz");
+            //}
+            //Console.Read();
+
+            #endregion
+        }
+    }
+}
